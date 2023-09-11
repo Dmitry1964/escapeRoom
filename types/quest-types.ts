d32e986d728bb22e2026a6fe83573e00a613d12f
Dmitry64 <nosdm64@gmail.com>
2023-09-11 20:47:13 +0300

    Добавил типы данных, получаемых с сервера

---
+++ types/quest-types.ts
@@ -0,0 +1,68 @@
+export type QuestType = {
+  id: string,
+  title: string,
+  previewImg: string,
+  previewImgWeb: string,
+  level: string,
+  type: string,
+  peopleMinMax: number[]
+}
+
+export type QuestInfoType = {
+  id: string,
+  title: string,
+  previewImg: string,
+  previewImgWeb: string,
+  level: string,
+  type: string,
+  peopleMinMax: number[],
+  descripions: string,
+  coverImg: string,
+  coverImgWeb: string
+}
+
+export type LocationType = {
+  address: string,
+  coords: number[]
+}
+
+export type AvailableType = {
+  time: string,
+  isAvailable: boolean
+}
+
+export type BookingInfoType = {
+  id: string,
+  location: LocationType,
+  slots: {
+    today: AvailableType[],
+    tomorrow: AvailableType[]
+  }
+}
+
+export type QuestBookingInfoType = {
+  date: string,
+  time: string,
+  contactPerson: string,
+  phone: string,
+  withChildren: boolean,
+  peopleCount: number,
+  placeId: string
+}
+
+export type UserBookingInfoType = {
+  date: string,
+  time: string,
+  contactPerson: string,
+  phone: string,
+  withChildren: boolean,
+  peopleCount: number,
+  id: string,
+  location: LocationType,
+  quest: QuestType
+}
+
+export type AythDataType= {
+  email: string,
+  password: string
+}
